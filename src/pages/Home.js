2b777125417d62adc26863832d939f41ed25f075
Jay Tejani <jaytejani12@gmail.com>
2022-03-02 13:53:07 +0530

    design complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,57 +2,89 @@ import React from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 import { Button, Container } from '@mui/material';
 import TodoModal from '../modal/TodoModal';
-
-
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
 
-
+    // const action = (params) => {
+    //     return (
+    //         <strong>
+    //             <Button
+    //                 variant="contained"
+    //                 color="primary"
+    //                 size="small"
+    //                 style={{ marginLeft: 16 }}
+    //             >
+    //                 More Info
+    //             </Button>
+    //         </strong>
+    //     )
+    // }
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        { field: 'date', headerName: 'Date', width: 130 },
+        { field: 'title', headerName: 'Title', width: 130 },
         {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 90,
+            field: 'description',
+            headerName: 'Description',
+            width: 350,
         },
         {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
+            field: 'status',
+            headerName: 'Status',
             width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+
         },
+        {
+            field: 'priority',
+            headerName: 'Priority',
+            width: 100,
+
+        },
+        {
+            field: "Action",
+            renderCell: () => {
+                return (
+                    <>
+                        <IconButton aria-label="edit" size="small">
+                            <EditIcon fontSize="inherit" onClick={handleOpen} />
+                        </IconButton>
+                        <IconButton aria-label="delete" size="small">
+                            <DeleteIcon fontSize="inherit" />
+                        </IconButton>
+                    </>
+                );
+            }
+        }
+
     ];
     const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, title: 'Create design', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 2, title: 'Lannister', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 3, title: 'Lannister', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 4, title: 'Stark', date: 'Arya', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 5, title: 'Targaryen', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 6, title: 'Melisandre', date: null, description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 7, title: 'Clifford', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 8, title: 'Frances', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
+        { id: 9, title: 'Roxie', date: '03/02/2022', description: 'To do task for details', status: 'Complete', priority: 'High' },
     ];
     return (
         <>
             <Container maxWidth="lg">
                 <Button variant="outlined" onClick={handleOpen} sx={{ m: 3 }}>Add</Button>
-                <div style={{ height: 400, width: '100%' }} sx={{ p: 3 }}>
+                <div style={{ height: 500, width: '100%' }} sx={{ p: 3 }}>
                     <DataGrid
                         rows={rows}
                         columns={columns}
-                        // pageSize={5}
-                        // rowsPerPageOptions={[5]}
+                    // pageSize={5}
+                    // rowsPerPageOptions={[5]}
                     />
                 </div>
                 <TodoModal open={open} setOpen={setOpen}></TodoModal>
